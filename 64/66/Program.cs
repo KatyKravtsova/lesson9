52e7020fc8bd16fe5926017f7a1da2fe9efd2495
Katy Kravtsova <kravtsovakaty@gmail.com>
2022-12-14 16:37:10 +0300

    66

---
+++ 64/66/Program.cs
@@ -0,0 +1,20 @@
+﻿// 66. Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine ("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+void GapNumberSum (int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    GapNumberSum(M, N, sum);
+}
+GapNumberSum (M, N, 0);
